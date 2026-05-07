d17061e26d8024aec7d2c3782676b0feacf45fce
JRABSCLL <juanrabascal@gmail.com>
2026-05-07 10:28:09 -0500

    feat: add flexible agency production route

---
+++ app/dashboard/agency-production-v2/page.tsx
@@ -0,0 +1,5 @@
+import { AgencyProductionModule } from "@/components/agency-production/agency-production-module"
+
+export default function AgencyProductionV2Page() {
+  return <AgencyProductionModule />
+}
